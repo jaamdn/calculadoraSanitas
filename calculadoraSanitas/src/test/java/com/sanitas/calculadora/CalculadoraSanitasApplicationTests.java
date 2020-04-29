ca936a39a1e7c83e18fe3f51327bee0d02420118
dvazquez <dvazquez@madpor1041.exis.local>
2020-04-29 16:55:57 +0200

    commit inicial

---
+++ calculadoraSanitas/src/test/java/com/sanitas/calculadora/CalculadoraSanitasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanitas.calculadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraSanitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
